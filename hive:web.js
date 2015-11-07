bff48445d414c21bdd46e235589bc8646ecddc83
Eric Martindale <eric@ericmartindale.com>
2015-11-06 16:51:37 -0800

    Implement basic Maki app.

--- hive:web.js
+++ hive:web.js
@@ -1,14 +1,3 @@
-var express = require('express');
-var app = express();
+var hive = require('./lib/hive');
 
-//Create a static file server
-app.configure(function() {
-  app.use(express.static(__dirname + '/public'));
-});
-
-//Get the dummy data
-require('./server/ddata.js');
-
-var port = 8080;
-app.listen(port);
-console.log('Express server started on port %s', port);
+hive.start();
