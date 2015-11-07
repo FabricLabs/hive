c9bdd5f5694e68091dc653ba6d18082f3c357a73
Adam Snodgrass <adam@virtualmed.org>
2015-11-06 22:35:50 -0600

    Add nodemon and watch gulp tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,6 +5,7 @@ import plugins from 'gulp-load-plugins';
 
 // live reload
 import sync from 'browser-sync';
+import http from 'http';
 
 // css processors
 import cssnext from 'cssnext';
@@ -17,6 +18,7 @@ import browserify from 'browserify';
 import babelify from 'babelify';
 import source from 'vinyl-source-stream';
 import buffer from 'vinyl-buffer';
+import assign from 'lodash.assign';
 
 const config = {
 	src: {
@@ -28,17 +30,25 @@ const config = {
 		scripts: 'public/js'
 	},
 	watch: {
-		styles: 'src/css/**/*.css'
+		styles: 'src/css/**/*.css',
+		scripts: 'src/js/**/*.js'
+	},
+	server: {
+		entry: 'hive:web.js',
+		host: 'localhost',
+		port: 6333
 	}
 };
 
 let g = plugins();
-let bundler = browserify({
+let opts = assign({}, watchify.args, {
 	entries: config.src.scripts,
 	debug: true
 });
+let bundler = browserify(opts);
 
 bundler.transform(babelify);
+bundler.on('log', g.util.log);
 
 function scripts() {
 	return bundler.bundle()
@@ -46,9 +56,10 @@ function scripts() {
 			g.util.log(err.toString());
 			this.emit('end');
 		})
-		.pipe(source('app.js'))
+		.pipe(source('hive.js'))
 		.pipe(buffer())
-		.pipe(gulp.dest(config.dest.scripts));
+		.pipe(gulp.dest(config.dest.scripts))
+		.pipe(sync.reload({stream: true}));
 }
 
 function styles() {
@@ -60,7 +71,8 @@ function styles() {
 
 	return gulp.src(config.src.styles)
 		.pipe(g.postcss(processors))
-		.pipe(gulp.dest(config.dest.styles));
+		.pipe(gulp.dest(config.dest.styles))
+		.pipe(sync.reload({stream: true}));
 }
 
 function build(done) {
@@ -70,6 +82,50 @@ function build(done) {
 	], done);
 }
 
+function nodemon(done) {
+	g.nodemon({
+		script: config.server.entry,
+		watch: ['lib/', 'resources/'],
+		ext: 'js html'
+	}).once('start', function() {
+		// the server might not be up so let's checkIfUp
+		checkIfUp(done);
+	});
+}
+
+function checkIfUp(done) {
+	let opts = {
+		hostname: config.server.host,
+		port: config.server.port,
+		path: '/',
+		method: 'GET'
+	};
+
+	// make HTTP requests until server responds
+	var req = http.request(opts, function () {
+		done();
+	});
+
+	req.on('error', () => checkIfUp(done));
+	req.end();
+}
+
+function watch(done) {
+	g.sequence('default', 'nodemon', function () {
+		bundler = watchify(bundler);
+		// bundler.on('update', scripts);
+
+		sync.init({
+			proxy: `${config.server.host}:${config.server.port}`
+		});
+
+		gulp.watch(config.watch.styles, ['styles']);
+		gulp.watch(config.watch.scripts, ['scripts']);
+	});
+}
+
 gulp.task('styles', styles);
 gulp.task('scripts', scripts);
+gulp.task('nodemon', nodemon);
 gulp.task('default', build);
+gulp.task('watch', watch);
