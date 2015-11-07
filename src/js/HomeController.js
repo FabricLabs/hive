1ba9b666163a1a1c748a81377c606f0550b72ebc
Brayton <brayton82@gmail.com>
2015-11-07 16:36:47 -0600

    Add mdBottomSheet

--- src/js/HomeController.js
+++ src/js/HomeController.js
@@ -1,8 +1,20 @@
 'use strict';
 
-class HomeController {
-  constructor($event) {
+const DIALOG = new WeakMap();
+const BOTTOMSHEET = new WeakMap();
+const EVENT = new WeakMap();
+const SIDENAV = new WeakMap();
 
+class HomeController {
+  constructor($event, $mdBottomSheet, $mdDialog, $mdSidenav) {
+    BOTTOMSHEET.set(this, $mdBottomSheet);
+    DIALOG.set(this, $mdDialog);
+    EVENT.set(this, $event);
+    SIDENAV.set(this, $mdSidenav);
+  }
+
+  toggleSidenav(menuId) {
+    $mdSidenav(menuId).toggle();
   }
 
   success(desserts) {
@@ -14,9 +26,9 @@ class HomeController {
     this.deferred = $nutrition.desserts.get($this.query, success).$promise;
   }
 
-  showListBottomSheet() {
+  showListBottomSheet($event) {
     this.alert = '';
-    $mdBottomSheet.show({
+    BOTTOMSHEET.get(this).show({
       template: '<md-bottom-sheet class="md-list md-has-header"> <md-subheader>Settings</md-subheader> <md-list> <md-item ng-repeat="item in items"><md-item-content md-ink-ripple flex class="inset"> <a flex aria-label="{{item.name}}" ng-click="listItemClick($index)"> <span class="md-inline-list-icon-label">{{ item.name }}</span> </a></md-item-content> </md-item> </md-list></md-bottom-sheet>',
       controller: 'HomeController',
       targetEvent: $event
@@ -28,8 +40,9 @@ class HomeController {
 }
 
 HomeController.$inject = [
-  '$mdBottomSheet',
-  '$event'
+  '$mdSidenav',
+  '$mdDialog',
+  '$mdBottomSheet'
 ];
 
 export default HomeController
