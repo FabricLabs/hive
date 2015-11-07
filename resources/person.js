521063389d1e4cab8d819585505735eaa8e24243
Eric Martindale <eric@ericmartindale.com>
2015-11-07 10:26:25 -0800

    tidy up user attributes

--- resources/person.js
+++ resources/person.js
@@ -2,12 +2,13 @@ var ObjectId = require('mongoose').SchemaTypes.ObjectId;
 
 var Person = {
   attributes: {
-    email: { type: String, unique: true, sparse: true },
-    username: { type: String },
+    email: { type: String , max: 240 , unique: true, sparse: true },
+    username: { type: String , max: 40 , slug: true },
+    password: { type: String , max: 240 , masked: true },
     roles: [{ type: String, enum: ['editor', 'moderator', 'admin'] }],
     created: { type: Date, default: Date.now, required: true },
     avatar: {
-      url: { type: String, default: '/img/user-avatar.png' }
+      url: { type: String , max: 240 , default: '/img/user-avatar.png' }
     },
     bio: { type: String, default: '' }
   }
