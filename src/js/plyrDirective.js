ac6bbf22f866d096487abf2861fc107ac062aa17
Adam Snodgrass <adam@virtualmed.org>
2015-11-07 03:35:21 -0600

    Add initial angular app and routing

---
+++ src/js/plyrDirective.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function plyrDirective() {
+  return {
+    template: '<div class="player"><div data-video-id="dQw4w9WgXcQ" data-type="youtube"></div></div>',
+    link: function (scope, el, attrs) {
+      plyr.setup();
+
+      document.querySelectorAll('.player')[0].plyr.play();
+    }
+  }
+}
+
+export default plyrDirective
