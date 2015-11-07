a96e1e0a02e1caf7ef36a274d8c07d04c262b66d
Adam Snodgrass <adam@virtualmed.org>
2015-11-06 21:34:56 -0600

    Add browserify scripts gulp task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -11,17 +11,44 @@ import cssnext from 'cssnext';
 import autoprefixer from 'autoprefixer';
 import atImport from 'postcss-import';
 
-let g = plugins();
+// browserify
+import watchify from 'watchify';
+import browserify from 'browserify';
+import babelify from 'babelify';
+import source from 'vinyl-source-stream';
+import buffer from 'vinyl-buffer';
+
 const config = {
 	src: {
-		styles: 'src/css/app.css'
+		styles: 'src/css/app.css',
+		scripts: ['src/js/app.js']
 	},
 	dest: {
-		styles: 'public/css'
+		styles: 'public/css',
+		scripts: 'public/js'
 	},
 	watch: {
 		styles: 'src/css/**/*.css'
 	}
+};
+
+let g = plugins();
+let bundler = browserify({
+	entries: config.src.scripts,
+	debug: true
+});
+
+bundler.transform(babelify);
+
+function scripts() {
+	return bundler.bundle()
+		.on('error', function () {
+			g.util.log(err.toString());
+			this.emit('end');
+		})
+		.pipe(source('app.js'))
+		.pipe(buffer())
+		.pipe(gulp.dest(config.dest.scripts));
 }
 
 function styles() {
@@ -38,9 +65,11 @@ function styles() {
 
 function build(done) {
 	g.sequence([
-		'styles'
+		'styles',
+		'scripts'
 	], done);
 }
 
 gulp.task('styles', styles);
+gulp.task('scripts', scripts);
 gulp.task('default', build);
