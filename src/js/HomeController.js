82cdaf2dcb86df3fbad545d2ee39a5736248f2e9
Brayton <brayton82@gmail.com>
2015-11-07 15:34:02 -0600

    Add search functions in HomeController

--- src/js/HomeController.js
+++ src/js/HomeController.js
@@ -1,7 +1,8 @@
 'use strict';
 
 class HomeController {
-  constructor() {
+  constructor($event) {
+
   }
 
   success(desserts) {
@@ -13,7 +14,7 @@ class HomeController {
     this.deferred = $nutrition.desserts.get($this.query, success).$promise;
   }
 
-  showListBottomSheet($event) {
+  showListBottomSheet() {
     this.alert = '';
     $mdBottomSheet.show({
       template: '<md-bottom-sheet class="md-list md-has-header"> <md-subheader>Settings</md-subheader> <md-list> <md-item ng-repeat="item in items"><md-item-content md-ink-ripple flex class="inset"> <a flex aria-label="{{item.name}}" ng-click="listItemClick($index)"> <span class="md-inline-list-icon-label">{{ item.name }}</span> </a></md-item-content> </md-item> </md-list></md-bottom-sheet>',
@@ -27,7 +28,8 @@ class HomeController {
 }
 
 HomeController.$inject = [
-  '$mdBottomSheet'
+  '$mdBottomSheet',
+  '$event'
 ];
 
 export default HomeController
