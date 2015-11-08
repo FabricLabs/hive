8f231ebcf6fb46c28d2c96966fe4791108acab76
Eric Martindale <eric@ericmartindale.com>
2015-11-07 17:09:22 -0800

    Working duration lookups.

--- resources/search.js
+++ resources/search.js
@@ -3,7 +3,7 @@ var ObjectId = require('mongoose').SchemaTypes.ObjectId;
 var Search = {
   attributes: {
     //created: { type: Date, required: true, default: Date.now },
-    query: { type: String , required: true }
+    query: { type: String , required: true , max: 80 }
   }
 }
 
