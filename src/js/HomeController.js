772ef291da32ed70291cf0a81876d741c9a21272
Brayton <brayton82@gmail.com>
2015-11-08 00:32:39 -0600

    Add style to channel list

--- src/js/HomeController.js
+++ src/js/HomeController.js
@@ -8,13 +8,13 @@ class HomeController {
   constructor($mdDialog, $mdBottomSheet, Channels) {
     BOTTOMSHEET.set(this, $mdBottomSheet);
     DIALOG.set(this, $mdDialog);
-    // EVENT.set(this, $event);
 
     this.channels = [];
+    this.imagePath = 'images/placeholder-360x.jpg';
 
     Channels.query((channels) => {
       this.channels = channels;
-      console.log(channels);
+      //console.log(channels);
     });
   }
 
@@ -39,6 +39,19 @@ class HomeController {
       targetEvent: $event
     }).then(function(clickedItem) {
       this.alert = clickedItem.name + ' clicked!';
+    });
+  }
+
+  showAdd(ev) {
+    DIALOG.get(this).show({
+      controller: 'HomeController',
+      template: '<md-dialog aria-label="User Form"> <md-content class="md-padding"> <form name="userForm"> <div layout layout-sm="column"> <md-input-container flex> <label>First Name</label> <input ng-model="user.firstName"> </md-input-container> <md-input-container flex> <label>Last Name</label> <input ng-model="theMax"> </md-input-container> </div> <md-input-container flex> <label>Address</label> <input ng-model="user.address"> </md-input-container> <div layout layout-sm="column"> <md-input-container flex> <label>City</label> <input ng-model="user.city"> </md-input-container> <md-input-container flex> <label>State</label> <input ng-model="user.state"> </md-input-container> <md-input-container flex> <label>Postal Code</label> <input ng-model="user.postalCode"> </md-input-container> </div> <md-input-container flex> <label>Biography</label> <textarea ng-model="user.biography" columns="1" md-maxlength="150"></textarea> </md-input-container> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button ng-click="answer(\'not useful\')"> Cancel </md-button> <md-button ng-click="answer(\'useful\')" class="md-primary"> Save </md-button> </div></md-dialog>',
+      targetEvent: ev,
+    })
+    .then(function(answer) {
+      this.alert = 'You said the information was "' + answer + '".';
+    }, function() {
+      this.alert = 'You cancelled the dialog.';
     });
   }
 
