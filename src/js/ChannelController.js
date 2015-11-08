f3f258fcf6d11216baea965f20f278922e0489ff
Adam Snodgrass <adam@virtualmed.org>
2015-11-08 00:10:37 -0600

    Fix channel styles and add responsive  video wrapper

--- src/js/ChannelController.js
+++ src/js/ChannelController.js
@@ -13,10 +13,20 @@ class ChannelController {
       let track = this.track = channel._track;
       this.name = channel.name;
 
-      Sources.get({_id: channel._track.sources}, sources => {
-        track.sources = sources;
-      });
+      if (track.sources) {
+        Sources.get({_id: channel._track.sources}, sources => {
+          track.sources = sources;
+        });
+      }
     });
+
+    var errorHandler = function() { ws = new WebSocket('wss://hive.media/channels/test'); }
+    var ws = new WebSocket('wss://hive.media/channels/test');
+    ws.onerror = errorHandler
+    ws.onmessage = function(msg) {
+      var data = JSON.parse(msg.data);
+      console.log(data.params || data);
+    };
   }
 }
 
