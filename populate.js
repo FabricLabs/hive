08b4441995b65627dd97238e6b71561ad25d5337
Eric Martindale <eric@ericmartindale.com>
2015-11-08 14:18:35 -0800

    Add sources, tracks to importer.

--- populate.js
+++ populate.js
@@ -8,6 +8,12 @@ var categoriesOnLocal = new Remote('http://localhost:6333/categories');
 var channelsOnProduction = new Remote('https://hive.media/channels');
 var channelsOnLocal = new Remote('http://localhost:6333/channels');
 
+var sourcesOnProduction = new Remote('https://hive.media/sources');
+var sourcesOnLocal = new Remote('http://localhost:6333/sources');
+
+var tracksOnProduction = new Remote('https://hive.media/tracks');
+var tracksOnLocal = new Remote('http://localhost:6333/tracks');
+
 categoriesOnProduction.query({}, function(cats) {
   async.each(cats, function(cat, done) {
     categoriesOnLocal.create({
@@ -24,6 +30,32 @@ categoriesOnProduction.query({}, function(cats) {
           }))
         }, function(err, channel) {
           console.log(err || channel);
+        });
+      });
+    });
+    
+    sourcesOnProduction.query({}, function(sources) {
+      sources.forEach(function(source) {
+        sourcesOnLocal.create({
+          name: source.name,
+          _category: _.sample(cats.map(function(x) {
+            return x._id;
+          }))
+        }, function(err, source) {
+          console.log(err || source);
+        });
+      });
+    });
+    
+    tracksOnProduction.query({}, function(tracks) {
+      tracks.forEach(function(track) {
+        tracksOnLocal.create({
+          name: track.name,
+          _category: _.sample(cats.map(function(x) {
+            return x._id;
+          }))
+        }, function(err, track) {
+          console.log(err || track);
         });
       });
     });
