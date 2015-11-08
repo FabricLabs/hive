4c739df2ee6b196a3721918fa7b9ccfffd1dbf91
Eric Martindale <eric@ericmartindale.com>
2015-11-07 22:25:15 -0800

    Allow searches to backpopulate.

--- resources/search.js
+++ resources/search.js
@@ -3,7 +3,9 @@ var ObjectId = require('mongoose').SchemaTypes.ObjectId;
 var Search = {
   attributes: {
     //created: { type: Date, required: true, default: Date.now },
-    query: { type: String , required: true , max: 80 }
+    query: { type: String , required: true , max: 80 },
+    _sources: [ { type: ObjectId , ref: 'Source' } ],
+    results: [ { type: ObjectId , ref: 'Track', populate: ['get'] } ],
   }
 }
 
